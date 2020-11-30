f9673f8a869238572929b539791c7b9a7abf5d9a
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2020-11-30 20:41:25 +0300

    Add async builders

---
+++ Packages/UGF.Builder/Runtime/IBuilderAsync.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace UGF.Builder.Runtime
+{
+    public interface IBuilderAsync
+    {
+        Task<T> BuildAsync<T>(object[] arguments);
+        Task<object> BuildAsync(object[] arguments);
+    }
+}
---
+++ Packages/UGF.Builder/Runtime/IBuilderAsync`2.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace UGF.Builder.Runtime
+{
+    public interface IBuilderAsync<in TArguments, TResult> : IBuilderAsync
+    {
+        Task<T> BuildAsync<T>(TArguments arguments) where T : TResult;
+        Task<TResult> BuildAsync(TArguments arguments);
+    }
+}
